2438a36e0d559d7aa2fe81c6da16bd2971700ba7
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-03-21 18:40:22 +0000

    added new const and var practice

---
+++ jsbasics01/constsandvars.js
@@ -0,0 +1,9 @@
+const accountId = 1443 //const keyword means it will never change in js//
+let accountEmail = "haider@gmail.com" //let and var both are used to store variable but var is old method so we always use let because it has larger scope//
+var accountPassword = "12345"
+accountCity = "jaipur"// in js we can also declare variable without 
+
+console.log(accountId); // this is a method to print the output in js
+
+// but what if we want many output to print at the same time? then we use,//
+console.table([accountEmail,accountPassword,accountCity])
