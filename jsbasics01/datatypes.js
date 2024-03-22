dcfed27c06df754e973ee1738f2bed10245e17ae
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-03-22 17:45:20 +0000

    datatypes

---
+++ jsbasics01/datatypes.js
@@ -0,0 +1,44 @@
+const score =100;
+const scoreValue =100.3
+ console.log(score);
+ console.log(typeof scoreValue)
+const isLoggedIn = false
+
+console.log(isLoggedIn)
+console.log(typeof isLoggedIn);
+let LoggedIn = true
+console.log(LoggedIn)
+console.log(typeof LoggedIn);
+const temperature = null;
+console.log(temperature);
+console.log(typeof temperature) //typeof of null value is object
+let boy;
+console.log(boy);
+console.log(typeof boy); // typeof undefined is undefined
+let names="haider"
+console.log(names);
+console.log(typeof names);
+
+//operations in js//
+console.log("1"+2) // -> 12
+console.log(1+"2") // -> 12
+console.log("1"+2+2) // ->122
+console.log(1+2+"2") // ->32 if operation is before then it is done and then string prints 
+
+//coversion of data types//
+let scores="34"
+let anotherScores= Number(scores)
+console.log(typeof anotherScores);
+
+let point=80
+let anotherPoint = String(point)
+console.log(typeof anotherPoint);
+
+let laptop="34ads"
+let anotherlaptop= Number(laptop)
+console.log(typeof anotherlaptop);
+
+
+
+let islogged =true
+console.log(typeof islogged);
