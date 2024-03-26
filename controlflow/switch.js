5c3afa3f0b051a0caf59d3886c7fb118f6d68ac4
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-03-26 10:31:05 +0000

    adding switch

---
+++ controlflow/switch.js
@@ -0,0 +1,30 @@
+// switch (key == wo value to check krna hai) {
+//     case value: // value will be strings also
+        
+//         break;
+
+//     default:
+//         break;
+// } syntax of switch 
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("March");
+        break;    
+    case 4: 
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+        
+        
+}
