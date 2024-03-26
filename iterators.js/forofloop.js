7a96d1cf793d2be093d8295fa3bfa098d68e197e
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-03-26 17:36:29 +0000

    for of loop

---
+++ iterators.js/forofloop.js
@@ -0,0 +1,31 @@
+// for of loop
+
+// on array
+// const arr = [1, 2, 3, 4, 5] 
+//  for (const num of arr) {
+//     console.log(num);
+//  }
+
+// on strings
+
+// const greetings = "Hello World!" 
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+// }
+
+// MAP
+// it outputs elements uniquely
+// const map = new Map()
+// map.set ('IN' , "India")
+// map.set ('USA' , "United states of america")
+// map.set ('Fr' , "France")
+// map.set ('IN' , "India")
+
+// console.log(map);
+
+// for (const [key , value] of map) {
+//     console.log(key , ':-', value);
+// }
+
+
+// for of loop will not work on objects with his method
