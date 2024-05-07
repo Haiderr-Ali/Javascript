8480f98aaddd511486fed617bc96fde8b4fd0165
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-05-07 20:12:00 +0530

    Update switch.js

--- controlflow/switch.js
+++ controlflow/switch.js
@@ -28,3 +28,47 @@ switch (month) {
         
         
 }
+
+
+
+// +++++++++++++++++++++++ TRUTHY AND FALSY VALUE ++++++++++++++++++++//
+
+/* FALSY VALUE => false, 0, -0, Bigint 0n, null, undefined, NaN
+
+TRUTHY VALUE => rest all the value another than this is a truthy value : e.g. "0", 'false', " ", [] , {} , function() {}
+*/
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+
+
+
+
+
+
