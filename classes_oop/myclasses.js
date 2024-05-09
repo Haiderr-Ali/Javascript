566624cd54c0de25c3642a9a00936326b24087df
Haider Ali <127727846+Haiderr-Ali@users.noreply.github.com>
2024-05-09 09:26:10 +0530

    Update myclasses.js

--- classes_oop/myclasses.js
+++ classes_oop/myclasses.js
@@ -14,7 +14,7 @@ class User {
         return `${this.username.toUpperCase()}`
     }
 
-}
+} // here in this code we created class constructor //
 
 const chai = new User("chai", "chai@gmail.com", "123")
 
